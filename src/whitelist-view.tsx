00790aaa2bd7b7b51f677af6fc108be19b77de43
Guide Fari <guideg6@gmail.com>
2024-12-11 06:51:32 +0200

    fix: redundant component. git gud.

--- src/whitelist-view.tsx
+++ src/whitelist-view.tsx
@@ -2,38 +2,35 @@ import { List, Icon, Color } from "@raycast/api";
 import { useLocalStorage } from "@raycast/utils";
 
 export default function WhitelistView() {
-  const { value: whitelistedApps, isLoading } = useLocalStorage<string[]>("whitelistedApps", []);
+	const { value: whitelistedApps, isLoading } = useLocalStorage<string[]>(
+		"whitelistedApps",
+		[],
+	);
+	console.log("whitelistedApps:", whitelistedApps);
 
-  return (
-    <List
-      isLoading={isLoading}
-      searchBarPlaceholder="Search whitelisted apps..."
-    >
-      <List.Section title="Whitelisted Apps" subtitle={`${whitelistedApps?.length || 0} apps`}>
-        {whitelistedApps?.map((app) => (
-          <List.Item
-            key={app}
-            title={app}
-            icon={{ fileIcon: `/Applications/${app}.app` }}
-            accessories={[
-              {
-                icon: { source: Icon.Shield, tintColor: Color.Green },
-                tooltip: "Whitelisted"
-              }
-            ]}
-          />
-        ))}
-        <List.Item
-          title="Raycast"
-          icon={{ fileIcon: "/Applications/Raycast.app" }}
-          accessories={[
-            {
-              icon: { source: Icon.Shield, tintColor: Color.Green },
-              tooltip: "Always Whitelisted"
-            }
-          ]}
-        />
-      </List.Section>
-    </List>
-  );
-} 
+	return (
+		<List
+			isLoading={isLoading}
+			searchBarPlaceholder="Search whitelisted apps..."
+		>
+			<List.Section
+				title="Whitelisted Apps"
+				subtitle={`${whitelistedApps?.length || 0} apps`}
+			>
+				{whitelistedApps?.map((app) => (
+					<List.Item
+						key={app}
+						title={app}
+						icon={{ fileIcon: `/Applications/${app}.app` }}
+						accessories={[
+							{
+								icon: { source: Icon.Shield, tintColor: Color.Green },
+								tooltip: "Whitelisted",
+							},
+						]}
+					/>
+				))}
+			</List.Section>
+		</List>
+	);
+}
